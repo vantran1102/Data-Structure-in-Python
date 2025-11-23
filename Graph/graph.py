209bedac3b59cfc5d4542586fed6a32302cb3e2e
vantran1102 <67261528+vantran1102@users.noreply.github.com>
2025-11-23 02:14:18 -0800

    update  graph

--- Graph/graph.py
+++ Graph/graph.py
@@ -9,3 +9,25 @@ class Graph:
     def print_graph(self):
         for vertex in self.adj_list:
             print(vertex, " :", self.adj_list[vertex])
+    def add_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    def remove_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    def remover_vertex(self,vertex):
+        if vertex in self.adj_list.keys():
+            for v in self.adj_list[vertex]:
+                self.adj_list[v].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
--- Graph/main.py
+++ Graph/main.py
@@ -2,4 +2,20 @@ from graph import Graph
 
 myGraph = Graph()
 myGraph.add_vertex("A")
+myGraph.add_vertex("B")
+myGraph.add_vertex("C")
+myGraph.add_vertex("D")
+print("Add Vertex")
+myGraph.print_graph()
+
+myGraph.add_edge('A','B')
+myGraph.add_edge('C','A')
+myGraph.add_edge('B','D')
+myGraph.add_edge('A','D')
+myGraph.add_edge('C','D')
+
+print("Add edge")
+myGraph.print_graph()
+print("Remove D")
+myGraph.remove_edge('A','D')
 myGraph.print_graph()
