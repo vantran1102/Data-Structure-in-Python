ab4210986de91ef14b31f430c77bd140bdcac7ad
vantran1102 <67261528+vantran1102@users.noreply.github.com>
2025-11-30 19:46:19 -0800

    Basic Sort

---
+++ Basic Sort/main.py
@@ -0,0 +1,9 @@
+from insertion_sort import insertion_sort
+from bubble_sort import bubble_sort
+from selection_sort import selection_sort
+my_list = [4,2,3,1,5,6]
+print("Bubble Sort: ",bubble_sort(my_list))
+my_list2 = [4,2,3,1,5,6]
+print("Selection Sort: ",selection_sort(my_list2))
+my_list3 = [4,2,3,1,5,6]
+print("Insertion Sort: ",insertion_sort(my_list3))
